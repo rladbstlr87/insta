3dbed4d05a01597fbc1420d8ba44521dc0662456
rladbstlr87 <manylong@naver.com>
2025-03-31 11:44:34 +0900

    enctype multipart/form-data

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 
@@ -11,3 +12,14 @@ def index(request):
     }
     return render(request, 'index.html', context)
 
+def create(request):
+    if request.method == 'POST':
+        pass
+    else:
+        form = PostForm()
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'create.html', context)
