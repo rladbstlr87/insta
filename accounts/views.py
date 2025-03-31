6a7d16ff14646ce3e1c957ae1d71654237bb4337
rladbstlr87 <manylong@naver.com>
2025-03-31 13:59:14 +0900

    signup 기능 구현

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('posts:index')
+    else:
+        form = CustomUserCreationForm()
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'signup.html', context)
+
+@login_required
+def login(request, id):
+    pass
