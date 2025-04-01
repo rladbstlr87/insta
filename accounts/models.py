531f0a993fb33987df6fabc3b0017feb1db15cdf
rladbstlr87 <manylong@naver.com>
2025-04-01 16:25:16 +0900

    imagekit으로 이미지 호환성 향상

--- accounts/models.py
+++ accounts/models.py
@@ -1,15 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django_resized import ResizedImageField
-
-# Create your models here.
+from imagekit.processors import ResizeToFill
+from imagekit.models import ProcessedImageField
 
 class User(AbstractUser):
-    profile_image = ResizedImageField(
-        size=[500, 500],
-        crop=['middle', 'center'],
-        upload_to='profile',
+    profile = models.ImageField(upload_to='profile')
+    profile_image = ProcessedImageField(
+        processors=[ResizeToFill(50, 50)],
+        format='JPEG',  # JPEG로 포맷 설정
+        options={'quality': 60},
+        upload_to='profile_image',
     )
+
     # post_set (FK)
     # comment_set
     # post_set => like_postes (MMF)
--- insta/settings.py
+++ insta/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'posts',
     'django_bootstrap5',
     'accounts',
+    'imagekit',
     
 ]
 
